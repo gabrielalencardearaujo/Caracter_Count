0861af487819959fd79b903b15e2f05cd6cea604
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2023-11-08 19:23:54 -0300

    get elements regex

--- src/components/Regex/main.ts
+++ src/components/Regex/main.ts
@@ -1,3 +1,35 @@
 export const start = () => {
+  const textarea = document.querySelector('#texto') as HTMLTextAreaElement;
+  const inputRegex = document.querySelector('#inputRegex') as HTMLInputElement;
+  const counter = document.querySelector('.counter span') as HTMLSpanElement;
+  const resultado = document.querySelector('.resultado') as HTMLDivElement;
+  const btnResult = document.querySelector('.btn') as HTMLButtonElement;
+  const alerta = document.querySelector('.alertas') as HTMLDivElement;
   
+  const handleClick = () => {
+    if(!textarea.value) {
+      alerta.textContent = 'Digite algum texto';
+      alerta.style.display = 'block'
+      return false;
+    } 
+
+    alerta.style.display = 'none';
+    return true;
+  }
+
+  const mountResult = () => {
+    const regexValue = inputRegex.value.split('/');
+    const mountRegex = new RegExp(regexValue[1], regexValue[2]);
+
+    // const replaceValues = textarea.value.replace(mountRegex, )
+  }
+
+  function spanRegex(children: string) {
+    return `<span>${children}</span>`
+  }
+
+  btnResult.addEventListener('click', () => {
+    const checkedInputs = handleClick();
+    if(checkedInputs) mountResult();
+  })
 }
