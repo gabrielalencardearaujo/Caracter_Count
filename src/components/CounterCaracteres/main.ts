035e0b868cffe90e2f18c288a6364f3a517b39cb
Gabriel Alencar de Araujo <dev.alencar.gabriel@gmail.com>
2023-11-08 17:59:15 -0300

    reorganization of files

---
+++ src/components/CounterCaracteres/main.ts
@@ -0,0 +1,42 @@
+import { searchedWord } from './searchedWord';
+import { allCaracter } from './allCaracter';
+import { allWords } from './allWords';
+
+export function start() {
+  const texto = document.querySelector('#texto') as HTMLTextAreaElement;
+  const btnContar = document.querySelector('.btn') as HTMLButtonElement;
+  const inputWord = document.querySelector('#consult') as HTMLInputElement;
+  const alertas = document.querySelector('.alertas') as HTMLDivElement;
+  const result = document.querySelector('.result') as HTMLDivElement;
+
+  btnContar.addEventListener('click', () => {
+    const checked = checkValues();
+    if (checked) mountResult();
+  });
+
+  function checkValues() {
+    if (!texto.value) {
+      alertas.style.display = 'block';
+      alertas.textContent = 'Digite algum texto';
+      return false;
+    }
+  
+    alertas.style.display = 'none';
+    return true;
+  }
+  
+  function mountResult() {
+    const regexMyWord = new RegExp(inputWord.value, 'gi');
+  
+    const myWordElement = searchedWord(texto.value.match(regexMyWord).length);
+    const numberCaracter = allCaracter(texto.value.length)
+    const numberWords = allWords(texto.value.match(/\w+/gi).length)
+  
+    result.innerHTML = `<h2>Resultados:</h2>`
+  
+    if (!inputWord.value)
+      result.innerHTML += numberCaracter + numberWords;
+    else
+      result.innerHTML += myWordElement + numberCaracter + numberWords;
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,43 +1,11 @@
 // import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import './assets/css/style.css';
-import { searchedWord } from './components/searchedWord';
-import { allCaracter } from './components/allCaracter';
-import { allWords } from './components/allWords';
+import * as Counter from './components/CounterCaracteres/main';
+import * as Regex from './components/Regex/main';
 
-const texto = document.querySelector('#texto') as HTMLTextAreaElement;
-const btnContar = document.querySelector('.btn') as HTMLButtonElement;
-const inputWord = document.querySelector('#consult') as HTMLInputElement;
-const alertas = document.querySelector('.alertas') as HTMLDivElement;
-const result = document.querySelector('.result') as HTMLDivElement;
+const urlSearchBar = window.location.href
 
-btnContar.addEventListener('click', () => {
-  const checked = checkValues();
-  if(checked) mountResult();
-});
+if(urlSearchBar.includes('index')) Counter.start();
 
-function checkValues() {
-  if(!texto.value) {
-    alertas.style.display = 'block';
-    alertas.textContent = 'Digite algum texto';
-    return false;
-  }
-
-  alertas.style.display = 'none';
-  return true;
-}
-
-function mountResult() {
-  const regexMyWord = new RegExp(inputWord.value, 'gi');
-
-  const myWordElement = searchedWord(texto.value.match(regexMyWord).length);
-  const numberCaracter = allCaracter(texto.value.length)
-  const numberWords = allWords(texto.value.match(/\w+/gi).length)
-
-  result.innerHTML = `<h2>Resultados:</h2>`
-
-  if(!inputWord.value) 
-    result.innerHTML += numberCaracter + numberWords;
-  else 
-    result.innerHTML += myWordElement + numberCaracter + numberWords;
-}
+if(urlSearchBar.includes('regex')) Regex.start();
